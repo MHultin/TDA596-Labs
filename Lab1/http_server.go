0683f43b63c88e66fcc250f2b508bb609432accb
Moltas Hultin <git@hultin.io>
2025-11-04 15:11:38 +0100

    Progress

--- Lab1/http_server.go
+++ Lab1/http_server.go
@@ -3,21 +3,18 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
-const MAX_CONNECTIONS int = 10
+// TODO: Implement check for valid http request
+// TODO: Write tests
 
-var acceptedExtensions = map[string]bool{
-	".html": true,
-	".css":  true,
-	".js":   true,
-	".jpg":  true,
-	".png":  true,
-}
+const MAX_CONNECTIONS int = 10
 
 func main() {
 
@@ -31,6 +28,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Println("Server started and listening on port " + port)
 	sem := make(chan struct{}, MAX_CONNECTIONS)
 
@@ -45,7 +43,6 @@ func main() {
 		go func(c net.Conn) {
 			defer func() {
 				<-sem
-				fmt.Println("Connection closed")
 			}()
 
 			handleConn(c)
@@ -53,53 +50,118 @@ func main() {
 	}
 }
 
+var acceptedExtensions = map[string]string{
+	".html": "text/html",
+	".css":  "text/css",
+	".txt":  "text/plain",
+	".jpg":  "image/jpeg",
+	".jpeg": "image/jpeg",
+	".gif":  "image/gif",
+}
+
 func handleConn(c net.Conn) {
 	defer c.Close()
 
 	br := bufio.NewReader(c)
 
-	line, err := br.ReadString('\n')
+	req, err := parseHTTPRequest(br)
+
+	fmt.Println(req.Method)
+	fmt.Println(req.RequestURI)
 
 	if err != nil {
-		fmt.Printf("Error reading request line: %v\n", err)
-		return
+		panic(err)
 	}
 
-	fmt.Println(line)
-
-	// TODO: Implement check for valid http request
-	// TODO: Implement 404 if requested file doesn't exist
-	// TODO: Implement 501 if the method is not GET or POST
-	// TODO: Write tests
-
-	splitRequest := strings.Split(line, " ")
-	method, path := splitRequest[0], splitRequest[1]
-
-	// TODO: Implement check for accepted file types (400 Bad Request)
-	extension := strings.ToLower(filepath.Ext(path))
-	if extension != "" && !acceptedExtensions[extension] {
-		fmt.Printf("Unsupported file type requested: %s\n", extension)
-		// If the extension is not in our map, send a 400 Bad Request
-		sendError(c, "400 Bad Request", fmt.Sprintf("Unsupported file type: %s", extension))
-		return
+	if !isAcceptedMethod(req.Method) {
+		sendError(c, "501", "501: Not implemented")
 	}
 
-	fmt.Println(method)
-	fmt.Println(path)
+	if req.Method == "GET" {
+		if !isAcceptedExtension(req.RequestURI) {
+			sendError(c, "400", "400: Bad Request")
+		}
 
-	fmt.Fprintf(c, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len("Test"), "Test")
+		handleGetRequest(c, req.RequestURI)
+	}
+	if req.Method == "POST" {
+		if req.RequestURI != "/upload" {
+			sendError(c, "400", "400: Bad Request")
+		}
+
+		handlePostRequest(c, req)
+	}
 }
 
-// Helper function to write an HTTP error response
+func handlePostRequest(c net.Conn, req *http.Request) {
+	if req.Body == nil {
+		sendError(c, "400", "No body provided")
+	}
+
+	data, err := io.ReadAll((req.Body))
+
+	fmt.Printf(req.Body)
+}
+
+func handleGetRequest(c net.Conn, path string) {
+	f, err := getFileBytes(path)
+	if err != nil {
+		sendError(c, "404", fmt.Sprintf("404: %s not found", path))
+	}
+
+	sendResponse(c, "200", f, acceptedExtensions[filepath.Ext(path)])
+}
+
+func isAcceptedExtension(path string) bool {
+	extension := strings.ToLower(filepath.Ext(path))
+	return !(extension == "" || acceptedExtensions[extension] == "")
+}
+
 func sendError(c net.Conn, status string, message string) {
-	// the response includesthe HTTP version, status code, and a body.
 	response := fmt.Sprintf("HTTP/1.1 %s\r\n", status)
 	response += "Content-Type: text/plain\r\n"
 	response += fmt.Sprintf("Content-Length: %d\r\n", len(message))
-	response += "Connection: close\r\n"
 	response += "\r\n"
 	response += message + "\n"
 
-	c.Write([]byte(response))
-	fmt.Printf("Sent error: %s - %s\n", status, message)
+	fmt.Fprint(c, response)
+}
+
+func sendResponse(c net.Conn, status string, body []byte, contentType string) {
+	response := fmt.Sprintf("HTTP/1.1 %s\r\n", status)
+	response += fmt.Sprintf("Content-Type: %s\r\n", contentType)
+	response += fmt.Sprintf("Content-Length: %d\r\n", len(body))
+	response += "\r\n"
+	response += string(body)
+
+	fmt.Fprint(c, response)
+}
+
+func getFileBytes(path string) ([]byte, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	p := filepath.Join(cwd, "public", path)
+
+	f, err := os.ReadFile(p)
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
+
+func isAcceptedMethod(method string) bool {
+	return method == "GET" || method == "POST"
+}
+
+func parseHTTPRequest(br *bufio.Reader) (*http.Request, error) {
+	req, err := http.ReadRequest(br)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
 }
