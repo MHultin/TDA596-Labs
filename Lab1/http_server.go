4791f29482fafd9e88b1f6801d5c0d8e4ff47fe6
Lucas Häyhänen <lucas.hayhanen@live.se>
2025-11-04 13:07:08 +0100

    TODO: Implement check for accepted file types (400 Bad Request)

--- Lab1/http_server.go
+++ Lab1/http_server.go
@@ -5,10 +5,19 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 const MAX_CONNECTIONS int = 10
+
+var acceptedExtensions = map[string]bool{
+	".html": true,
+	".css":  true,
+	".js":   true,
+	".jpg":  true,
+	".png":  true,
+}
 
 func main() {
 
@@ -49,10 +58,15 @@ func handleConn(c net.Conn) {
 
 	br := bufio.NewReader(c)
 
-	line, _ := br.ReadString('\n')
+	line, err := br.ReadString('\n')
+
+	if err != nil {
+		fmt.Printf("Error reading request line: %v\n", err)
+		return
+	}
+
 	fmt.Println(line)
 
-	// TODO: Implement check for accepted file types (400 Bad Request)
 	// TODO: Implement check for valid http request
 	// TODO: Implement 404 if requested file doesn't exist
 	// TODO: Implement 501 if the method is not GET or POST
@@ -61,8 +75,31 @@ func handleConn(c net.Conn) {
 	splitRequest := strings.Split(line, " ")
 	method, path := splitRequest[0], splitRequest[1]
 
+	// TODO: Implement check for accepted file types (400 Bad Request)
+	extension := strings.ToLower(filepath.Ext(path))
+	if extension != "" && !acceptedExtensions[extension] {
+		fmt.Printf("Unsupported file type requested: %s\n", extension)
+		// If the extension is not in our map, send a 400 Bad Request
+		sendError(c, "400 Bad Request", fmt.Sprintf("Unsupported file type: %s", extension))
+		return
+	}
+
 	fmt.Println(method)
 	fmt.Println(path)
 
 	fmt.Fprintf(c, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len("Test"), "Test")
+}
+
+// Helper function to write an HTTP error response
+func sendError(c net.Conn, status string, message string) {
+	// the response includesthe HTTP version, status code, and a body.
+	response := fmt.Sprintf("HTTP/1.1 %s\r\n", status)
+	response += "Content-Type: text/plain\r\n"
+	response += fmt.Sprintf("Content-Length: %d\r\n", len(message))
+	response += "Connection: close\r\n"
+	response += "\r\n"
+	response += message + "\n"
+
+	c.Write([]byte(response))
+	fmt.Printf("Sent error: %s - %s\n", status, message)
 }
